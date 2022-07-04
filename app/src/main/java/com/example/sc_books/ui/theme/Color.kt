9cd24224510acec17546c93fcc7bac708b4e159f
Xiomara <xjaquehua@unsa.edu.pe>
2022-07-04 01:27:57 -0500

    gradle

--- app/src/main/java/com/example/sc_books/ui/theme/Color.kt
+++ app/src/main/java/com/example/sc_books/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.sc_books.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Purple200 = Color(129,212,250)
+val Purple500 = Color(3,169,244)
+val Purple700 = Color(2,136,209)
 val Teal200 = Color(0xFF03DAC5)
-val CustomViolet = Color(142,125,190)
+val CustomViolet = Color(79,195, 247)
 val LB50_400 = Color(0xFF29B6F6)
 val LB50_900 = Color(0xFF01579B)
